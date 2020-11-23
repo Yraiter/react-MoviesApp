4a07efcca1a98e0deadb22991181289d2cf9cf39
Yraiter <yairraiter@gmail.com>
2020-11-23 20:35:55 +0200

    commit all cahnges

--- src/containers/MainPage/MainPage.js
+++ src/containers/MainPage/MainPage.js
@@ -7,56 +7,56 @@ import Sidebar from '../Sidebar/Sidebar';
 import Header from '../Header/Header';
 import './MainPage.css';
 import axios from 'axios'
-// class MainPage extends Component {
+class MainPage extends React.Component {
 
 
-//     render() {
-//         return (
-//             <>
-//                 <section>
-//                     <Header />
-//                 </section>
-//                 <section className="main">
-//                     <Sidebar />
-//                     <Movies />
-//                 </section>
-//             </>
-//         );
-//     }
-// }
-
-// export default MainPage;
-
-
-
-
-
-function MainPage() {
-
-    const [optionsGeners, setOptionGenre] = useState()
-    useEffect(() => {
-        axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US`)
-            .then((response) => {
-                const options = response.data.genres.map(genre => {
-                    return { key: genre.id, value: genre.name, label: genre.name }
-                })
-                setOptionGenre(options)
-            })
-            .catch((error) => console.log("this is an " + error))
-        console.log("Sidebar useEffect")
-    }, [])
-
-    return (
-        <>
-            <section>
-                <Header />
-            </section>
-            <section className="main">
-                <Sidebar options={optionsGeners} />
-                <Movies />
-            </section>
-        </>
-    );
+    render() {
+        return (
+            <>
+                <section>
+                    <Header />
+                </section>
+                <section className="main">
+                    <Sidebar />
+                    <Movies />
+                </section>
+            </>
+        );
+    }
 }
 
-export default MainPage
+export default MainPage;
+
+
+
+
+
+// function MainPage() {
+
+//     const [optionsGeners, setOptionGenre] = useState()
+//     useEffect(() => {
+//         axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US`)
+//             .then((response) => {
+//                 const options = response.data.genres.map(genre => {
+//                     return { key: genre.id, value: genre.name, label: genre.name }
+//                 })
+//                 setOptionGenre(options)
+//             })
+//             .catch((error) => console.log("this is an " + error))
+//         console.log("Sidebar useEffect")
+//     }, [])
+
+//     return (
+//         <>
+//             <section>
+//                 <Header />
+//             </section>
+//             <section className="main">
+//                 <Sidebar options={optionsGeners} />
+//                 <Movies />
+//             </section>
+//         </>
+//     );
+// }
+
+// export default MainPage
--- src/containers/Sidebar/Sidebar.js
+++ src/containers/Sidebar/Sidebar.js
@@ -1,35 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Select from 'react-select'
 // // import AsyncSelect from 'react-select/async';
 import makeAnimated from 'react-select/animated';
-// import axios from 'axios'
+import axios from 'axios'
 
 import './Sidebar.css'
 
-const Sidebar = (props) => {
-    // const [genre, setgenre] = useState()
-    const animatedComponents = makeAnimated();
-    // const selectRef = useRef()
-    // useEffect(() => {
-    //     axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US`)
-    //         .then((response) => {
-    //             const options = response.data.genres.map(genre => {
-    //                 return { key: genre.id, value: genre.name, label: genre.name }
-    //             })
-    //             setgenre(options)
-    //         })
-    //         .catch((error) => console.log("this is an " + error))
-    //     console.log("Sidebar useEffect")
-    //     return () => {
-    //         selectRef()
-    //     }
-    // }, [])
+const Sidebar = () => {
+    const newLocal = makeAnimated();
+    const [genre, setgenre] = useState()
+    const animatedComponents = newLocal;
+
+    useEffect(() => {
+        axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=ab2fefad2fb133b8288873e93a86f02e&language=en-US`)
+            .then((response) => {
+                const options = response.data.genres.map(genre => {
+                    return { key: genre.id, value: genre.name, label: genre.name }
+                })
+                setgenre(options)
+                console.log(genre)
+            })
+            .catch((error) => console.log("this is an " + error))
+        console.log("Sidebar useEffect")
+
+    }, [])
 
     return (
         <div className="sidebar">
             <Select
                 isMulti
-                options={props.options}
+                options={genre}
                 components={animatedComponents}
             />
         </div>
